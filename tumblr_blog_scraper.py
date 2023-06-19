a9c1a0ffb74dd0a0fb77c6356705117f249c0ab5
Chase Galloway <chase.h.galloway21@gmail.com>
2023-06-19 14:42:19 -0400

    Wrote code to remove any post that failed to be saved in the .csv file

--- tumblr_blog_scraper.py
+++ tumblr_blog_scraper.py
@@ -7,6 +7,12 @@ import time
 # Enter your Tumblr API key here:
 tumblr = pytumblr.TumblrRestClient('')
 
+# Set search parameters here:
+#
+# Tumblr's API only supports searching for a single blog at a time
+# Filter options are text, html, or raw
+# Before - only search for posts before this time - in seconds past the epoch
+# use calendar.timegm(time.gmtime()) to use the current epoch
 blog_url = ''  # Tumblr blog URL (e.g., example.tumblr.com)
 filter = 'text'
 before = calendar.timegm(time.gmtime())
@@ -22,6 +28,10 @@ with open(filePath, mode='a', newline='', encoding='utf-8') as results_file:
 print("Searching Tumblr for 100 posts from blog '" + blog_url + "'")
 
 j = 1
+
+# If you really wanted to, you could increase this number here to increase the number of batches
+# Tumblr's API limits you to 20 results at a time, but we can loop through the search multiple times
+# BUT I'd be cautious about making this number too big - to avoid running into API call limits
 
 while j < 5:
     # Run the search and snag the results
@@ -46,13 +56,14 @@ while j < 5:
         except KeyError:
             body = "Couldn't Get Post Body"
 
-        with open(filePath, mode='a', newline='', encoding='utf-8') as results_file:
-            results_writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
-            try:
-                results_writer.writerow([date, url, blog_name, title, tags, body])
-                print("Successfully wrote row for post from " + date)
-            except:
-                print("Error writing row")
+        if body != "Couldn't Get Post Body":
+            with open(filePath, mode='a', newline='', encoding='utf-8') as results_file:
+                results_writer = csv.writer(results_file, delimiter=',', quotechar='"', quoting=csv.QUOTE_ALL)
+                try:
+                    results_writer.writerow([date, url, blog_name, title, tags, body])
+                    print("Successfully wrote row for post from " + date)
+                except:
+                    print("Error writing row")
 
     # Next, we make note of the oldest timestamp
     oldestTime = post["timestamp"]
