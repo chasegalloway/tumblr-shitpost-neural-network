67841f667eb1884071039527db327400b9ec2be3
Chase Galloway <chase.h.galloway21@gmail.com>
2023-06-19 20:36:54 -0400

    Separated the model from the generation, rewrote the model to be trained based off of .txt files instead of .csv

--- model.py
+++ model.py
@@ -4,20 +4,16 @@ from tensorflow.keras.models import Sequential
 from tensorflow.keras.preprocessing.text import Tokenizer
 from tensorflow.keras.preprocessing.sequence import pad_sequences
 import numpy as np
-import pandas as pd
-import glob
 
-# Step 1: Data Collection and Preprocessing
-# Read data from multiple CSV files
-file_pattern = 'TumblrPosts-1687204101-pukicho.csv'
-csv_files = glob.glob(file_pattern)
+# Read data from a text file
+text_file = 'TumblrPosts-1687218509-pukicho.txt'
 
-posts = []
-for file in csv_files:
-    data = pd.read_csv(file)
-    posts.extend(data['body'].tolist())
+with open(text_file, 'r', encoding='utf-8') as f:
+    text_data = f.read()
 
-# Step 2: Tokenization and Sequence Generation
+# Preprocessing
+posts = text_data.split('\n')
+
 tokenizer = Tokenizer()
 tokenizer.fit_on_texts(posts)
 total_words = len(tokenizer.word_index) + 1
@@ -38,35 +34,13 @@ input_sequences = np.array(input_sequences)
 X = input_sequences[:, :-1]
 y = input_sequences[:, -1]
 
-# Step 3: Neural Network Model
 model = Sequential()
 model.add(Embedding(total_words, 100, input_length=max_sequence_len-1))
 model.add(LSTM(150))
 model.add(Dense(total_words, activation='softmax'))
 model.compile(loss='categorical_crossentropy', optimizer='adam')
 
-# Step 4: Training the Model
 history = model.fit(X, tf.keras.utils.to_categorical(y, num_classes=total_words),
                     epochs=50, verbose=1)
 
-# Step 5: Saving the Model
 model.save('tumblr_model.h5')
-
-# Step 6: Generating Text
-seed_text = "Your starting prompt"
-next_words = 100
-
-for _ in range(next_words):
-    token_list = tokenizer.texts_to_sequences([seed_text])[0]
-    token_list = pad_sequences([token_list], maxlen=max_sequence_len-1, padding='pre')
-    predicted = np.argmax(model.predict(token_list), axis=-1)
-    output_word = ""
-    for word, index in tokenizer.word_index.items():
-        if index == predicted:
-            output_word = word
-            break
-    seed_text += " " + output_word
-
-# Step 7: Saving the Generated Text
-with open('generated_text.txt', 'w') as f:
-    f.write(seed_text)
